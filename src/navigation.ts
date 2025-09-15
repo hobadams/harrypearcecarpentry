417b767229ac684fe66090eaffd5e391b0ffb917
Hob Adams <hob__adams@hotmail.com>
2025-09-15 12:36:24 +0100

    handle active link

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,6 +1,6 @@
 export const navigation = [
-  { href: "/", label: "Home" },
-  { href: "/services", label: "Services" },
-  { href: "/contact", label: "Contact" },
-  { href: "/gallery", label: "Gallery" },
+  { href: "/", label: "Home", exactMatch: true },
+  { href: "/services", label: "Services", exactMatch: false },
+  { href: "/contact", label: "Contact", exactMatch: false },
+  { href: "/gallery", label: "Gallery", exactMatch: false },
 ];
