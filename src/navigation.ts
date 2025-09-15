03311b8437b1c45712ba16050e710a94f4074850
Hob Adams <hob__adams@hotmail.com>
2025-09-15 10:27:57 +0100

    add mobile menu

---
+++ src/navigation.ts
@@ -0,0 +1,6 @@
+export const navigation = [
+  { href: "/", label: "Home" },
+  { href: "/services", label: "Services" },
+  { href: "/contact", label: "Contact" },
+  { href: "/gallery", label: "Gallery" },
+];
